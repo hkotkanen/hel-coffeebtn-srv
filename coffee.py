33d8827ffd520ed08a521c4fe60484986e6d2522
Henri Kotkanen <henri.kotkanen@gmail.com>
2016-08-26 14:24:56 +0300

    added rpi button script, changes to endpoints

--- coffee.py
+++ coffee.py
@@ -1,18 +1,26 @@
-from flask import Flask, request, redirect, jsonify, g
+from flask import Flask, request, redirect, jsonify, g, render_template
 import arrow
 
 app = Flask(__name__)
-db_file = 'data/coffees.db'
+db_file = 'coffees.db'
 
-@app.route('/coffees', methods=['GET', 'POST'])
+@app.route('/api/coffees', methods=['GET', 'POST'])
 def coffees():
     ct = arrow.now('Europe/Helsinki')
     if request.method == 'GET':
         return jsonify({'latest': stamper.get_latest()})
 
     if request.method == 'POST':
+        # save current time in db as the latest
         stamper.push_new_ts(ct.isoformat())
+        # send message to slack?
+        # send message to yammer?
+        # maybe send message to all connected browser websockets?
         return jsonify({'latest': stamper.get_latest()})
+
+@app.route('/coffee', methods=['GET'])
+def render_coffees():
+    return render_template('coffee.html', latest=stamper.get_latest())
 
 
 class TimeStamper():
@@ -27,9 +35,9 @@ class TimeStamper():
                 with open(self.db_file, 'r') as db:
                     print('fetching latest from file')
                     self.latest_ts = db.readlines()[-1]
-            except:
-                print('exception in open text file, returning current time')
-                self.latest_ts = arrow.now('Europe/Helsinki').isoformat()
+            except IOError:
+                print('Couldn\'t open db file.')
+                self.latest_ts = "2000-01-01"
         return self.latest_ts
 
     def push_new_ts(self, iso_ts):
---
+++ rpi/button.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+
+# IF ESPEAK FAILS, MUST DO "amixer cset numid=3 1" (1=3.5mm jack, 2=HDMI)
+
+import sys, os
+import RPIO as GPIO
+import requests
+import arrow
+import time
+
+input_ch = 2
+
+prev_input = 0
+prev_ts = 0
+button_depressed = False
+timedelta = 3
+
+def get_latest_and_speak():
+    try:
+        resp = requests.get('http://sanchopanza.local:5000/api/coffees')
+        prev_coffee = arrow.get(resp.json()['latest'])
+        os.system('espeak -v fi "{}"'.format('Kahvi on valmistettu ' + prev_coffee.humanize(locale='fi')))
+    except:
+        os.system('espeak -v fi "{}"'.format('En tiedä milloin kahvi on valmistettu. Palvelin ei vastaa.'))
+    finally:
+        time.sleep(2)
+
+def post_made_coffee():
+    try:
+        resp = requests.post('http://sanchopanza.local:5000/api/coffees')
+        os.system('espeak -v fi "{}"'.format('Rekisteröity kahvin uuttamisen aloitus.'))
+    except:
+        os.system('espeak -v fi "{}"'.format('Ei saatu yhteyttä palvelimeen.'))
+    finally:
+        time.sleep(2)
+
+if __name__=="__main__":
+    print('setting up')
+    # GPIO.setmode(GPIO.BCM)
+    GPIO.setup(input_ch, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+    try:
+        while True:
+            input = GPIO.input(input_ch)
+
+            # so here we look for the edges manually and sleep a bit to try to ignore the bouncing
+
+            # going from 0 -> 1: BUTTON WENT DOWN
+            if (not prev_input) and input and (not button_depressed):
+                print('button down')
+                button_depressed = True
+                prev_ts = time.perf_counter()
+
+            # button is at 1 (pressed down).
+            # if it's been down long enough, POST to server
+            elif input and button_depressed:
+                # down long enough?
+                if (time.perf_counter() - prev_ts > timedelta):
+                    button_depressed = False
+                    # print('REGISTERED')
+                    post_made_coffee()
+                    time.sleep(1)
+
+            # going from 1 -> 0: BUTTON WENT UP...
+            # ...before time was up, so just GET the latest timestamp
+            elif (not input) and prev_input and button_depressed:
+                button_depressed = False
+                if (time.perf_counter() - prev_ts <= timedelta):
+                    # print('button up SHORT')
+                    get_latest_and_speak()
+
+            prev_input = input
+            time.sleep(0.1)
+
+    except KeyboardInterrupt:
+        print('interrupted!')
+        GPIO.cleanup()
